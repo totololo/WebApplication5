4748ce306ae682fdd08e25aed543a10d1639ad53
LEURQUIN Maxence <mleurquin@nortia.fr>
2017-04-11 12:26:37 +0200

    teststststststtst

--- WebApplication5/WebForm1.aspx.cs
+++ WebApplication5/WebForm1.aspx.cs
@@ -7,15 +7,10 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication5
 {
-    public partial class About : Page
+    public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-
-
-
-
 
         }
     }
